0275286fedeaef7bdf36a7aa1809b75748bdf565
alxn787 <alenabraham787@gmail.com>
2026-06-17 12:06:43 +0530

    expiry active and passive

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ use std::io::{Read, Write};
 use std::process::Command;
 use std::ptr::null;
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
 fn main() { 
 
-    let map: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
+    let map: Arc<Mutex<HashMap<String, (String, Option<Instant>)>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    let map_clone = map.clone();
+    std::thread::spawn(move || {cleanup_expired_keys(map_clone)});
     
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
@@ -25,7 +29,7 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream, map: Arc<Mutex<HashMap<String, String>>>) {
+fn handle_connection(mut stream: TcpStream, map: Arc<Mutex<HashMap<String, (String, Option<Instant>)>>>) {
     let mut buffer = [0; 1024];
     loop {        
         let bytes_len = stream.read(&mut buffer).unwrap();
@@ -33,7 +37,7 @@ fn handle_connection(mut stream: TcpStream, map: Arc<Mutex<HashMap<String, Strin
             break;
         }
         let request = String::from_utf8_lossy(&buffer[..bytes_len]);
-        let (command, key, value) = parse_command(&request);
+        let (command, key, value, expiry_type, expiry_value) = parse_command(&request);
     
         match command {
             "PING" => {
@@ -45,13 +49,35 @@ fn handle_connection(mut stream: TcpStream, map: Arc<Mutex<HashMap<String, Strin
             }
             "SET" => {
                 let mut map = map.lock().unwrap();
-                map.insert(key.to_string(), value.to_string());
-                stream.write_all(b"+OK\r\n".as_ref()).unwrap();
+                if expiry_type == "PX" {
+                    let expiry_value = expiry_value.parse::<u64>().unwrap();
+                    let expiry_time = std::time::Instant::now() + std::time::Duration::from_millis(expiry_value);
+                    map.insert(key.to_string(), (value.to_string(), Some(expiry_time)));
+                } else if expiry_type == "EX" {
+                    let expiry_value = expiry_value.parse::<u64>().unwrap();
+                    let expiry_time = std::time::Instant::now() + std::time::Duration::from_secs(expiry_value);
+                    map.insert(key.to_string(), (value.to_string(), Some(expiry_time)));
+                } else {
+                    map.insert(key.to_string(), (value.to_string(), None));
+                }
+                let response = format!("+OK\r\n");
+                stream.write_all(response.as_bytes()).unwrap();
             }
             "GET" => {
-                let map = map.lock().unwrap();
-                let value = map.get(key).unwrap_or(&String::new()).to_string();
-                let response = format!("${}\r\n{}\r\n", value.len(), value);
+                let mut map = map.lock().unwrap();
+
+                let value = match map.get(key) {
+                    Some((value, expiry_time)) => (value.to_string(), expiry_time),
+                    None => (String::new(), &None),
+                };
+                if let Some(expiry_time) = value.1 {
+                    if *expiry_time < std::time::Instant::now() {
+                        map.remove(key);
+                        stream.write_all(b"$-1\r\n".as_ref()).unwrap();
+                        return;
+                    }
+                }
+                let response = format!("${}\r\n{}\r\n", value.0.len(), value.0);
                 stream.write_all(response.as_bytes()).unwrap();
             }
             _ => {
@@ -61,10 +87,32 @@ fn handle_connection(mut stream: TcpStream, map: Arc<Mutex<HashMap<String, Strin
     }
 }
 
-fn parse_command(command: &str) -> (&str, &str, &str) {
+fn parse_command(command: &str) -> (&str, &str, &str, &str, &str) {
     let commands = command.split("\r\n").collect::<Vec<&str>>();
     let command = commands.get(2).copied().unwrap_or("");
     let key = commands.get(4).copied().unwrap_or("");
     let value = commands.get(6).copied().unwrap_or("");
-    (command, key, value)
+    let expiry_type = commands.get(8).copied().unwrap_or("");
+    let expiry_value = commands.get(10).copied().unwrap_or("");
+    (command, key, value, expiry_type, expiry_value)
+}
+
+fn cleanup_expired_keys(map: Arc<Mutex<HashMap<String, (String, Option<Instant>)>>>) {
+    loop {
+        {
+            let mut map = map.lock().unwrap();
+            let now = std::time::Instant::now();
+
+            map.retain(|_, (_, expiry_time)| {
+                if let Some(expiry_time) = expiry_time {
+                    if *expiry_time < now {
+                        return false;
+                    }
+                }
+                true
+            });
+        }
+        
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }   
 }
